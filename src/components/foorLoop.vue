575a64aa7152ae6145e7e67629a5e0b4a575a20c
Kanon11 <kanonshet@yahoo.com>
2022-08-12 14:17:13 +0600

    fc

---
+++ src/components/foorLoop.vue
@@ -0,0 +1,26 @@
+<template>
+  <table border="2px solid">
+    <th>id</th>
+    <th>name</th>
+    <th>age</th>
+    <tr v-for="user in users" :key="user.id">
+    <td>{{user.id}}</td>
+    <td>{{user.name}}</td>
+    <td>{{user.age}}</td>
+    </tr>
+  </table>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        { name: "kanon", id: 1, age: 30 },
+        { name: "kpol", id: 2, age: 22 },
+        { name: "ayon", id: 3, age: 17 },
+        { name: "mukti", id: 4, age: 30 },
+      ],
+    };
+  },
+};
+</script>
